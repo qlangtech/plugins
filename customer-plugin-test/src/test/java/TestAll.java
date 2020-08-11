46bdc3c41b12e7dd0c500ffc647883702f261f4d
莫正华 <mozhenghua@mozhenghuadeMacBook-Pro.local>
2020-08-11 13:12:30 +0800

    modify incr script

---
+++ customer-plugin-test/src/test/java/TestAll.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author: baisui 百岁
+ * @create: 2020-08-11 11:10
+ **/
+public class TestAll {
+}
---
+++ tis-asyncmsg-rocketmq-plugin/src/test/java/com/qlangtech/tis/mq/TestConsumer.java
@@ -0,0 +1,8 @@
+package com.qlangtech.tis.mq;
+
+/**
+ * @author: baisui 百岁
+ * @create: 2020-08-06 14:53
+ **/
+public class TestConsumer {
+}

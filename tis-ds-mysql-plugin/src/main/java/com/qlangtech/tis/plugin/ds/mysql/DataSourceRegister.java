9e4cee32904e889b39e36e9c8126ea51ba5edafb
莫正华 <mozhenghua@mozhenghuadeMacBook-Pro.local>
2020-12-04 09:41:18 +0800

    make new pluggable datasoure mysql dump success

--- tis-ds-mysql-plugin/src/main/java/com/qlangtech/tis/plugin/ds/mysql/DataSourceRegister.java
+++ tis-ds-mysql-plugin/src/main/java/com/qlangtech/tis/plugin/ds/mysql/DataSourceRegister.java
@@ -1,14 +1,14 @@
 /**
  * Copyright (c) 2020 QingLang, Inc. <baisui@qlangtech.com>
- *
+ * <p>
  * This program is free software: you can use, redistribute, and/or modify
  * it under the terms of the GNU Affero General Public License, version 3
  * or later ("AGPL"), as published by the Free Software Foundation.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,9 +20,7 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -34,19 +32,19 @@ import java.sql.Statement;
  * @author 百岁（baisui@qlangtech.com）
  * @date 2016年1月17日 下午2:45:47
  */
-public class DataSourceRegister implements ApplicationContextAware, InitializingBean {
+public class DataSourceRegister {
 
-    private static final Logger log = LoggerFactory.getLogger(com.qlangtech.tis.manage.common.DataSourceRegister.class);
+    private static final Logger log = LoggerFactory.getLogger(DataSourceRegister.class);
 
-    private String processDbs;
-
-    public String getProcessDbs() {
-        return processDbs;
-    }
-
-    public void setProcessDbs(String processDbs) {
-        this.processDbs = processDbs;
-    }
+//    private String processDbs;
+//
+//    public String getProcessDbs() {
+//        return processDbs;
+//    }
+//
+//    public void setProcessDbs(String processDbs) {
+//        this.processDbs = processDbs;
+//    }
 
     public abstract static class DBRegister {
 
@@ -86,65 +84,65 @@ public class DataSourceRegister implements ApplicationContextAware, Initializing
         }
     }
 
-    private ApplicationContext applicationContext;
+    //private ApplicationContext applicationContext;
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        this.applicationContext = applicationContext;
+//    }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        // 校验bean 是否正确
-        String[] dbs = StringUtils.split(processDbs, ";");
-        DataSource ds = null;
-        StringBuffer buffer = new StringBuffer();
-        for (String db : dbs) {
-            ds = (DataSource) applicationContext.getBean(db);
-            if (ds == null) {
-                throw new IllegalStateException("db:" + db + " is not defined");
-            }
-            if (!isOK(ds, buffer)) {
-                buffer.append(db).append(",");
-            }
-        }
-        if (buffer.length() > 0) {
-            log.error("db config has some error," + buffer);
-            throw new IllegalStateException("db has some error:" + buffer.toString());
-        }
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        // 校验bean 是否正确
+//        String[] dbs = StringUtils.split(processDbs, ";");
+//        DataSource ds = null;
+//        StringBuffer buffer = new StringBuffer();
+//        for (String db : dbs) {
+//            ds = (DataSource) applicationContext.getBean(db);
+//            if (ds == null) {
+//                throw new IllegalStateException("db:" + db + " is not defined");
+//            }
+//            if (!isOK(ds, buffer)) {
+//                buffer.append(db).append(",");
+//            }
+//        }
+//        if (buffer.length() > 0) {
+//            log.error("db config has some error," + buffer);
+//            throw new IllegalStateException("db has some error:" + buffer.toString());
+//        }
+//    }
 
     private boolean hasRrcordDbError = false;
 
-    /**
-     * @param ds
-     * @throws SQLException
-     */
-    private boolean isOK(DataSource ds, StringBuffer errorRecord) throws SQLException {
-        try {
-            Connection conn;
-            Statement statement;
-            ResultSet result;
-            conn = ds.getConnection();
-            statement = conn.createStatement();
-            result = statement.executeQuery("select 1");
-            if (result.next()) {
-                if (result.getInt(1) != 1) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-            result.close();
-            statement.close();
-            conn.close();
-            return true;
-        } catch (Exception e) {
-            if (!hasRrcordDbError) {
-                errorRecord.append(ExceptionUtils.getStackTrace(e));
-                hasRrcordDbError = true;
-            }
-            return false;
-        }
-    }
+//    /**
+//     * @param ds
+//     * @throws SQLException
+//     */
+//    private boolean isOK(DataSource ds, StringBuffer errorRecord) throws SQLException {
+//        try {
+//            Connection conn;
+//            Statement statement;
+//            ResultSet result;
+//            conn = ds.getConnection();
+//            statement = conn.createStatement();
+//            result = statement.executeQuery("select 1");
+//            if (result.next()) {
+//                if (result.getInt(1) != 1) {
+//                    return false;
+//                }
+//            } else {
+//                return false;
+//            }
+//            result.close();
+//            statement.close();
+//            conn.close();
+//            return true;
+//        } catch (Exception e) {
+//            if (!hasRrcordDbError) {
+//                errorRecord.append(ExceptionUtils.getStackTrace(e));
+//                hasRrcordDbError = true;
+//            }
+//            return false;
+//        }
+//    }
 }

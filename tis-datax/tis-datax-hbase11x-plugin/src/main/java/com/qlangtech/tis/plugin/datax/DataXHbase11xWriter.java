bb364ed7c40d15ccdee55f6db7648ea5e7d4ced4
mozhenghua <baisui@qlangtech.com>
2021-12-07 14:01:14 +0800

    update version 3.3.0 and modify license to apache2

--- tis-datax/tis-datax-hbase11x-plugin/src/main/java/com/qlangtech/tis/plugin/datax/DataXHbase11xWriter.java
+++ tis-datax/tis-datax-hbase11x-plugin/src/main/java/com/qlangtech/tis/plugin/datax/DataXHbase11xWriter.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (c) 2020 QingLang, Inc. <baisui@qlangtech.com>
- * <p>
- *   This program is free software: you can use, redistribute, and/or modify
- *   it under the terms of the GNU Affero General Public License, version 3
- *   or later ("AGPL"), as published by the Free Software Foundation.
- * <p>
- *  This program is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *   FITNESS FOR A PARTICULAR PURPOSE.
- * <p>
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 package com.qlangtech.tis.plugin.datax;
 
@@ -47,7 +33,7 @@ public class DataXHbase11xWriter extends DataxWriter {
     public String encoding;
         @FormField(ordinal = 4, type = FormFieldType.INPUTTEXT, validate = { })
     public String column;
-    
+
     @FormField(ordinal = 5, type = FormFieldType.TEXTAREA, validate = {Validator.require})
     public String template;
 
--- tis-datax/tis-hive-flat-table-builder-plugin/src/main/java/com/qlangtech/tis/fullbuild/taskflow/hive/HiveLocalExec.java
+++ tis-datax/tis-hive-flat-table-builder-plugin/src/main/java/com/qlangtech/tis/fullbuild/taskflow/hive/HiveLocalExec.java
@@ -1,16 +1,19 @@
 /**
- * Copyright (c) 2020 QingLang, Inc. <baisui@qlangtech.com>
- * <p>
- *   This program is free software: you can use, redistribute, and/or modify
- *   it under the terms of the GNU Affero General Public License, version 3
- *   or later ("AGPL"), as published by the Free Software Foundation.
- * <p>
- *  This program is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *   FITNESS FOR A PARTICULAR PURPOSE.
- * <p>
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package com.qlangtech.tis.fullbuild.taskflow.hive;
 
@@ -31,12 +34,12 @@ public class HiveLocalExec {
     // cmdLine.addArgument(
     // "select count(1) from totalpay where pt='20160224001002';",
     // true);
-    // 
+    //
     // System.out.println("start===================");
-    // 
+    //
     // // CommandLine cmdLine = new CommandLine("/bin/sh");
     // // cmdLine.addArgument("./dumpcenter-daily.sh");
-    // 
+    //
     // CommandLine cmdLine = new CommandLine("hive");
     // cmdLine.addArgument("--database");
     // cmdLine.addArgument("tis");
@@ -44,25 +47,25 @@ public class HiveLocalExec {
     // cmdLine.addArgument(
     // "select count(1) from instance;\n select count(1) from totalpay;",
     // true);
-    // 
+    //
     // System.out.println("getExecutable:" + cmdLine.getExecutable());
     // System.out.println(cmdLine.getArguments());
     // System.out.println("==============");
     // DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
-    // 
+    //
     // ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
     // DefaultExecutor executor = new DefaultExecutor();
     // executor.setWorkingDirectory(new File("/home/baisui/tis"));
-    // 
+    //
     // executor.setStreamHandler(new PumpStreamHandler(System.out));
     // executor.setExitValue(1);
     // executor.setWatchdog(watchdog);
     // executor.execute(cmdLine, resultHandler);
-    // 
+    //
     // // 等待5个小时
     // resultHandler.waitFor(5 * 60 * 60 * 1000);
     // System.out.println("exec over===================");
-    // 
+    //
     // System.out.println("exitCode:" + resultHandler.getExitValue());
     // if (resultHandler.getException() != null) {
     // resultHandler.getException().printStackTrace();

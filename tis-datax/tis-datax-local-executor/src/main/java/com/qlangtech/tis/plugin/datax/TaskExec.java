d6fe93ff4d637a7a37461bce07a0278f0a6bc964
mozhenghua <baisui@qlangtech.com>
2021-12-22 09:55:41 +0800

    remote zookeeper dependency

--- tis-datax/tis-datax-local-executor/src/main/java/com/qlangtech/tis/plugin/datax/TaskExec.java
+++ tis-datax/tis-datax-local-executor/src/main/java/com/qlangtech/tis/plugin/datax/TaskExec.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.qlangtech.tis.plugin.datax;
@@ -43,12 +43,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
  **/
 public class TaskExec {
     private static final Logger logger = LoggerFactory.getLogger(TaskExec.class);
-    private static final ExecutorService dataXExecutor = newFixedThreadPool(10);// Executors.newCachedThreadPool();
 
 
     public static ExecutorService newFixedThreadPool(int nThreads) {
         return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>(40),
+                new LinkedBlockingQueue<Runnable>(30),
                 Executors.defaultThreadFactory());
     }
 
@@ -60,6 +59,7 @@ public class TaskExec {
         return new IRemoteJobTrigger() {
             DataXJobSingleProcessorExecutor jobConsumer;
             boolean hasCanceled;
+            private final ExecutorService dataXExecutor = newFixedThreadPool(10);// Executors.newCachedThreadPool();
 
             @Override
             public void submitJob() {
@@ -127,8 +127,17 @@ public class TaskExec {
                         }
                     } finally {
                         complete.set(true);
+                        shutdownExecutor();
                     }
                 });
+            }
+
+            private void shutdownExecutor() {
+                try {
+                    dataXExecutor.shutdownNow();
+                } catch (Throwable e) {
+                    logger.error(e.getMessage(), e);
+                }
             }
 
             @Override
@@ -140,6 +149,7 @@ public class TaskExec {
                     watchdog.destroyProcess();
                     logger.info("taskId:{} relevant task has been canceled", taskId);
                 });
+                shutdownExecutor();
                 this.hasCanceled = true;
             }
 

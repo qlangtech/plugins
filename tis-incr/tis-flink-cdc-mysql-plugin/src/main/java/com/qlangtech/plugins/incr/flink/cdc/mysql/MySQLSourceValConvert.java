31ca48298ae5495634143a41caf3f97f7af6be86
mozhenghua <baisui@qlangtech.com>
2021-12-30 17:34:06 +0800

    timestamp format from 'yyyy-MM-dd'T'HH:mm:ss'Z'' to yyyy-MM-dd HH:mm:ss

--- tis-incr/tis-flink-cdc-mysql-plugin/src/main/java/com/qlangtech/plugins/incr/flink/cdc/mysql/MySQLSourceValConvert.java
+++ tis-incr/tis-flink-cdc-mysql-plugin/src/main/java/com/qlangtech/plugins/incr/flink/cdc/mysql/MySQLSourceValConvert.java
@@ -47,7 +47,8 @@ public class MySQLSourceValConvert implements ISourceValConvert, Serializable {
     public static final ThreadLocal<SimpleDateFormat> TIME_FORMAT = new ThreadLocal<SimpleDateFormat>() {
         @Override
         protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            //return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         }
     };
     private TabColIndexer colIndexer;

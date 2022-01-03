2bf6a8a374d3a7a2283c3b5de5bb1888dff9ad67
mozhenghua <baisui@qlangtech.com>
2022-01-03 15:18:22 +0800

    modify version to 3.4.0

--- tis-datax/tis-hive-flat-table-builder-plugin/src/test/java/TestAll.java
+++ tis-datax/tis-hive-flat-table-builder-plugin/src/test/java/TestAll.java
@@ -1,24 +1,23 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import com.qlangtech.tis.dump.hive.TestHiveDBUtils;
 import com.qlangtech.tis.dump.hive.TestHiveRemoveHistoryDataTask;
-import com.qlangtech.tis.fullbuild.indexbuild.impl.TestYarnTableDumpFactory;
 import com.qlangtech.tis.hdfs.impl.TestHdfsFileSystemFactory;
 import com.qlangtech.tis.hive.TestDefaultHiveConnGetter;
 import com.qlangtech.tis.hive.TestHiveInsertFromSelectParser;
@@ -45,7 +44,7 @@ public class TestAll extends TestCase {
         suite.addTestSuite(TestDataXHiveWriter.class);
         suite.addTestSuite(TestHiveInsertFromSelectParser.class);
         suite.addTestSuite(TestHiveDBUtils.class);
-        suite.addTestSuite(TestYarnTableDumpFactory.class);
+        // suite.addTestSuite(TestYarnTableDumpFactory.class);
         return suite;
     }
 }
--- tis-incr/tis-flink-cdc-mysql-plugin/src/test/java/com/qlangtech/plugins/incr/flink/cdc/mysql/TestFlinkCDCMySQLSourceFactory.java
+++ tis-incr/tis-flink-cdc-mysql-plugin/src/test/java/com/qlangtech/plugins/incr/flink/cdc/mysql/TestFlinkCDCMySQLSourceFactory.java
@@ -18,54 +18,87 @@
 
 package com.qlangtech.plugins.incr.flink.cdc.mysql;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.qlangtech.plugins.incr.flink.TISFlinClassLoaderFactory;
 import com.qlangtech.tis.coredefine.module.action.TargetResName;
 import com.qlangtech.tis.datax.IDataxProcessor;
-import com.qlangtech.tis.datax.impl.DataxReader;
 import com.qlangtech.tis.manage.common.CenterResource;
 import com.qlangtech.tis.plugin.datax.SelectedTab;
 import com.qlangtech.tis.plugin.datax.common.BasicDataXRdbmsReader;
+import com.qlangtech.tis.plugin.ds.BasicDataSourceFactory;
+import com.qlangtech.tis.plugin.ds.DBConfig;
 import com.qlangtech.tis.plugin.ds.ISelectedTab;
 import com.qlangtech.tis.plugin.incr.TISSinkFactory;
 import com.qlangtech.tis.realtime.BasicFlinkSourceHandle;
 import com.qlangtech.tis.realtime.transfer.DTO;
 import com.qlangtech.tis.test.TISEasyMock;
-import junit.framework.TestCase;
+import com.ververica.cdc.connectors.mysql.testutils.MySqlContainer;
+import org.apache.flink.runtime.minicluster.RpcServiceSharing;
+import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.test.util.MiniClusterWithClientResource;
 import org.easymock.EasyMock;
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.lifecycle.Startables;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * @author: 百岁（baisui@qlangtech.com）
  * @create: 2021-12-18 09:57
  **/
-public class TestFlinkCDCMySQLSourceFactory extends TestCase implements TISEasyMock {
+public class TestFlinkCDCMySQLSourceFactory implements TISEasyMock {
+    private static final Logger LOG = LoggerFactory.getLogger(TestFlinkCDCMySQLSourceFactory.class);
+    protected static final int DEFAULT_PARALLELISM = 4;
+    protected static final MySqlContainer MYSQL_CONTAINER =
+            (MySqlContainer)
+                    new MySqlContainer()
+                            .withConfigurationOverride("docker/server-gtids/my.cnf")
+                            .withSetupSQL("docker/setup.sql")
+                            .withDatabaseName("flink-test")
+                            .withUsername("flinkuser")
+                            .withPassword("flinkpw")
+                            .withLogConsumer(new Slf4jLogConsumer(LOG));
 
-    @Override
+    @Rule
+    public final MiniClusterWithClientResource miniClusterResource =
+            new MiniClusterWithClientResource(
+                    new MiniClusterResourceConfiguration.Builder()
+                            .setNumberTaskManagers(1)
+                            .setNumberSlotsPerTaskManager(DEFAULT_PARALLELISM)
+                            .setRpcServiceSharing(RpcServiceSharing.DEDICATED)
+                            .withHaLeadershipControl()
+                            .build());
+
+    @BeforeClass
+    public static void startContainers() {
+        LOG.info("Starting containers...");
+        Startables.deepStart(Stream.of(MYSQL_CONTAINER)).join();
+        LOG.info("Containers are started.");
+    }
+
+    @Before
     public void setUp() throws Exception {
         CenterResource.setNotFetchFromCenterRepository();
         System.setProperty(TISFlinClassLoaderFactory.SKIP_CLASSLOADER_FACTORY_CREATION, "true");
     }
 
-    public void testDate() {
 
-        long date = 1639831884000l;
-
-
-        System.out.println(MySQLSourceValConvert.TIME_FORMAT.get().format(new Date(date)));
-
-    }
-
+    @Test
     public void testBinlogConsume() throws Exception {
 
 
         FlinkCDCMySQLSourceFactory mysqlCDCFactory = new FlinkCDCMySQLSourceFactory();
         mysqlCDCFactory.startupOptions = "latest";
         final String tabName = "base";
-        TargetResName dataxName = new TargetResName("mysql_startrock2");
+        TargetResName dataxName = new TargetResName("x");
 
         BasicFlinkSourceHandle consumerHandle = new TestBasicFlinkSourceHandle(tabName);
 
@@ -82,24 +115,36 @@ public class TestFlinkCDCMySQLSourceFactory extends TestCase implements TISEasyM
 
         FlinkCDCMysqlSourceFunction imqListener = (FlinkCDCMysqlSourceFunction) mysqlCDCFactory.create();
 
+        // DataxReader.IDataxReaderGetter readerGetter = mock("IDataxReaderGetter", DataxReader.IDataxReaderGetter.class);
+        BasicDataXRdbmsReader dataxReader = this.mock("dataxReader", BasicDataXRdbmsReader.class);
 
-        BasicDataXRdbmsReader dataxReader = (BasicDataXRdbmsReader) DataxReader.load(null, dataxName.getName());
-        Objects.requireNonNull(dataxReader);
+        BasicDataSourceFactory dataSourceFactory = new BasicDataSourceFactory() {
+            @Override
+            public String buidJdbcUrl(DBConfig db, String ip, String dbName) {
+                // return MYSQL_CONTAINER.getJdbcUrl();
+                throw new UnsupportedOperationException();
+            }
+        };
+
+        dataSourceFactory.dbName = MYSQL_CONTAINER.getDatabaseName();
+        dataSourceFactory.nodeDesc = MYSQL_CONTAINER.getHost();
+        dataSourceFactory.password = MYSQL_CONTAINER.getPassword();
+        dataSourceFactory.userName = MYSQL_CONTAINER.getUsername();
+        dataSourceFactory.port = MYSQL_CONTAINER.getDatabasePort();
+
+        EasyMock.expect(dataxReader.getDataSourceFactory()).andReturn(dataSourceFactory);
+        // EasyMock.expect(readerGetter.get(dataxName.getName())).andReturn(dataxReader);
+
+        //  DataxReader.dataxReaderGetter = readerGetter;
+        // BasicDataXRdbmsReader dataxReader = (BasicDataXRdbmsReader) DataxReader.load(null, dataxName.getName());
+        // Objects.requireNonNull(dataxReader);
 //        IDataxReader rdbmsReader =
 
         List<SelectedTab> selectedTabs = dataxReader.getSelectedTabs();
         Optional<SelectedTab> firstSelectedTab = selectedTabs.stream().filter((t) -> tabName.equals(t.name)).findFirst();
-        assertTrue("firstSelectedTab must be present", firstSelectedTab.isPresent());
+        Assert.assertTrue("firstSelectedTab must be present", firstSelectedTab.isPresent());
 
-         ISelectedTab tab = firstSelectedTab.get();
-//        EasyMock.expect(tab.getName()).andReturn(tabName).anyTimes();
-//        List<ISelectedTab.ColMeta> cols = Lists.newArrayList();
-//        ISelectedTab.ColMeta col = new ISelectedTab.ColMeta();
-//        col.setName("base_id");
-//        col.setPk(true);
-//        col.setType(DataType.dat);
-//        cols.add(col);
-
+        ISelectedTab tab = firstSelectedTab.get();
 
 
         List<ISelectedTab> tabs = Collections.singletonList(tab);

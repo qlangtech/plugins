ef1bfcdaf98af5cdbc9d43d61097744d379edc48
mozhenghua <baisui@qlangtech.com>
2022-02-09 19:55:48 +0800

    add

--- tis-datax/tis-datax-hdfs-plugin/src/main/java/com/qlangtech/tis/plugin/datax/BasicFSWriter.java
+++ tis-datax/tis-datax-hdfs-plugin/src/main/java/com/qlangtech/tis/plugin/datax/BasicFSWriter.java
@@ -96,93 +96,7 @@ public abstract class BasicFSWriter extends DataxWriter implements KeyedPluginSt
     }
 
 
-    protected static SupportHiveDataType convert2HiveType(ColumnMetaData.DataType type) {
-        Objects.requireNonNull(type, "param type can not be null");
 
-        return type.accept(new ColumnMetaData.TypeVisitor<SupportHiveDataType>() {
-            @Override
-            public SupportHiveDataType intType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.INT;
-            }
-
-            @Override
-            public SupportHiveDataType floatType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.FLOAT;
-            }
-
-            @Override
-            public SupportHiveDataType decimalType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.DOUBLE;
-            }
-
-            @Override
-            public SupportHiveDataType timeType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.TIMESTAMP;
-            }
-
-            @Override
-            public SupportHiveDataType tinyIntType(ColumnMetaData.DataType dataType) {
-                return SupportHiveDataType.TINYINT;
-            }
-
-            @Override
-            public SupportHiveDataType smallIntType(ColumnMetaData.DataType dataType) {
-                return SupportHiveDataType.SMALLINT;
-            }
-
-            @Override
-            public SupportHiveDataType longType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.BIGINT;
-            }
-
-            @Override
-            public SupportHiveDataType doubleType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.DOUBLE;
-            }
-
-            @Override
-            public SupportHiveDataType dateType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.DATE;
-            }
-
-            @Override
-            public SupportHiveDataType timestampType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.TIMESTAMP;
-            }
-
-            @Override
-            public SupportHiveDataType bitType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.BOOLEAN;
-            }
-
-            @Override
-            public SupportHiveDataType blobType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.STRING;
-            }
-
-            @Override
-            public SupportHiveDataType varcharType(ColumnMetaData.DataType type) {
-                return SupportHiveDataType.STRING;
-            }
-        });
-
-//        switch (type.getCollapse()) {
-//            case Long:
-//                return SupportHiveDataType.BIGINT;
-//            case Double:
-//                return SupportHiveDataType.DOUBLE;
-//            case STRING:
-//                return SupportHiveDataType.STRING;
-//            case Boolean:
-//                return SupportHiveDataType.BOOLEAN;
-//            case Date:
-//                return SupportHiveDataType.DATE;
-//            case INT:
-//                return SupportHiveDataType.INT;
-//            default:
-//                throw new IllegalStateException("illeal type:" + type);
-//        }
-    }
 
 
     @Override
@@ -228,7 +142,7 @@ public abstract class BasicFSWriter extends DataxWriter implements KeyedPluginSt
             return this.tabMap.getSourceCols().stream().map((c) -> {
                 HiveColumn col = new HiveColumn();
                 col.setName(c.getName());
-                col.setType(convert2HiveType(c.getType()).name());
+                col.setType(SupportHiveDataTypeUtils.convert2HiveType(c.getType()).name());
                 return col;
             }).collect(Collectors.toList());
         }
